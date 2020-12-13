1d145a96b0aae5d4ddfa8796ea43a1ccfdc0a89b
Stephen Razis <srazis@yahoo.com>
2020-12-13 13:14:29 -0800

    updated my database schema to include logs and to better represent project specifications

--- database_script/BankOfByte.sql
+++ database_script/BankOfByte.sql
@@ -15,25 +15,34 @@ create table users (
 );
 
 
+create table customer (
+	user_id int unique not null references users (user_id),
+	customer_id serial primary key
+);
+
+
 create table account (
 	date_created timestamp not null,
 	balance numeric(10,2) not null,
 	is_validated boolean not null,
-	user_id int not null references users (user_id),
+	customer_id int not null references customer (customer_id),
 	account_id serial primary key
-);
-
-
-create table customer (
-	user_id int unique not null references users (user_id),
-	account_id int unique not null references account (account_id),
-	primary key (user_id, account_id)
 );
 
 
 create table employee (
 	user_id int unique not null references users (user_id),
 	employee_id serial primary key
+);
+
+
+create table transaction_logs (
+	transaction_id serial primary key,
+	transaction_type text not null,
+	amount numeric(10,2) not null,
+	from_account int references account (account_id),
+	to_account int references account (account_id),
+	is_finished boolean not null
 );
 
 
@@ -53,33 +62,44 @@ insert into employee (user_id) values (3);
 --dummy customers
 insert into users (user_password, first_name, last_name, birth_date, email, phone, address)
 			values ('Password123!', 'Julius', 'Caesar', '0051-01-01', 'julius@julioclaudian.com', 11111111, '1 Forum Drive');
-insert into account (date_created, balance, is_validated, user_id)
-			values ('2020-01-01', 2000.00, true, 4);
-insert into customer (user_id, account_id)
-			values (4, 1);
+insert into customer (user_id)
+			values (4);
+insert into account (date_created, balance, is_validated, customer_id)
+			values ('2020-01-01', 2000.00, true, 1);
 insert into users (user_password, first_name, last_name, birth_date, email, phone, address)
 			values ('Password123!', 'Augustus', 'Caesar', '0027-01-01', 'augustus@julioclaudian.com', 11111111, '1 Forum Drive');
-insert into account (date_created, balance, is_validated, user_id)
-			values ('2020-01-01', 0.00, false, 5);
-insert into customer (user_id, account_id)
-			values (5, 2);
+insert into customer (user_id)
+			values (5);
+insert into account (date_created, balance, is_validated, customer_id)
+			values ('2020-01-01', 0.00, false, 2);
 insert into users (user_password, first_name, last_name, birth_date, email, phone, address)
 			values ('Password123!', 'Tiberius', 'Caesar', '0014-01-01', 'tiberius@julioclaudian.com', 11111111, '1 Forum Drive');
-insert into account (date_created, balance, is_validated, user_id)
-			values ('2020-01-01', 1000.00, true, 6);
-insert into customer (user_id, account_id)
-			values (6, 3);
-insert into account (date_created, balance, is_validated, user_id)
+insert into customer (user_id)
+			values (6);
+insert into account (date_created, balance, is_validated, customer_id)
+			values ('2020-01-01', 1000.00, true, 3);
+		
+--employee that is also a customer with an account
+insert into customer (user_id)
+			values (1);
+insert into account (date_created, balance, is_validated, customer_id)
+			values ('2020-12-12', 500.00, true, 4);
+
+--customer with 2 accounts
+insert into account (date_created, balance, is_validated, customer_id)
 			values ('2020-12-12', 500.00, true, 1);
-insert into customer (user_id, account_id)
-			values (1, 4);
+
+
+insert into transaction_logs (transaction_type, amount, from_account, is_finished)
+			values ('WITHDRAW', 100.00, 1, true);
+insert into transaction_logs (transaction_type, amount, to_account, is_finished)
+			values ('DEPOSIT', 100.00, 5, true);
+insert into transaction_logs (transaction_type, amount, from_account, to_account, is_finished)
+			values ('TRANSFER', 100.00, 1, 5, false);
+		
 commit;
 
-
+		
 select * from users;
 
-select * from customer;
-
-select * from account;
-
-select * from employee;
+select * from transaction_logs;
